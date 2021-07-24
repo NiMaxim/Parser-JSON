77f272cee2a67d9c16f6d68910fef789d3e114cc
NiMaxim <75665121+NiMaxim@users.noreply.github.com>
2021-07-24 16:20:02 +0700

    Add files

---
+++ parser.js
@@ -0,0 +1,15 @@
+function readFile(input) {
+  let file = input.files[0];
+
+  let reader = new FileReader();
+
+  reader.readAsText(file);
+
+  reader.onload = function () {
+    console.log(JSON.parse(reader.result));
+  };
+
+  reader.onerror = function () {
+    console.log(reader.error);
+  };
+}
