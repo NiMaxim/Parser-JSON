b812984763028e5ae1aff1ac793482ee4cffac04
NiMaxim <75665121+NiMaxim@users.noreply.github.com>
2021-07-25 20:59:42 +0700

    Добавлена проверка ключей и очистка содержимого

--- parser.js
+++ parser.js
@@ -1,3 +1,5 @@
+//Проверка загружаемого JSON на наличие ключей
+
 function readFile(input) {
   let file = input.files[0];
 
@@ -6,10 +8,51 @@ function readFile(input) {
   reader.readAsText(file);
 
   reader.onload = function () {
-    console.log(JSON.parse(reader.result));
+    let jsonResult = JSON.parse(reader.result);
+    console.log(jsonResult);
+
+    for (let key in jsonResult) {
+      switch (key) {
+        case "name":
+          console.log("NAME имеется");
+          document.querySelector(
+            ".content"
+          ).innerHTML = `<div class="name"></div>`;
+          let jsonName = document.createElement("h2");
+          jsonName.innerHTML = JSON.stringify(jsonResult["name"]);
+          document.querySelector(".name").appendChild(jsonName);
+
+          break;
+
+        case "fields":
+          console.log("FIELDS имеется");
+          break;
+
+        case "references":
+          console.log("REFERENCES имеется");
+          break;
+
+        case "buttons":
+          console.log("BUTTONS имеется");
+          break;
+
+        default:
+          break;
+      }
+    }
   };
 
   reader.onerror = function () {
     console.log(reader.error);
   };
+}
+
+//Очистка содержимого
+
+let node = document.getElementById("content_box");
+let resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", resetFunction);
+
+function resetFunction() {
+  node.innerHTML = "";
 }
